e4e60b100042107026087b6aff54c5c6c5f3917f
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-02-22 17:07:30 +0200

    initial commit + solution + server setup with grpc

---
+++ GrpcServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog((hostingContext, loggerConfiguration) =>
+            {
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+            });
+    }
+}
